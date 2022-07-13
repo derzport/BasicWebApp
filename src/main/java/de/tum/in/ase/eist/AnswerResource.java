9cae1cebb802c3490f319040a989c64445e97bd8
jorgemoreira <jorgemoreira421@gmail.com>
2022-07-13 16:51:02 +0200

    ada

--- src/main/java/de/tum/in/ase/eist/AnswerResource.java
+++ src/main/java/de/tum/in/ase/eist/AnswerResource.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnswerResource {
 
 	//wazzup
-
+	//adaw
 
 
 	private final Logger logger = LoggerFactory.getLogger(AnswerResource.class);
