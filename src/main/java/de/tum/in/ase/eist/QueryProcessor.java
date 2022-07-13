127f369361551626549966f15881b81f2f816cca
jorgemoreira <jorgemoreira421@gmail.com>
2022-07-13 17:24:04 +0200

    ada

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class QueryProcessor {
 
+    private int[]a;
+    int total;
     public String process(String query) {
 		query = query.toLowerCase();
         if (query.contains("shakespeare")) {
@@ -13,8 +15,26 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Jorge";
-        } else { // TODO extend the programm here
+        } else {
+            getLargest(a,total);// TODO extend the programm here
             return  "";
         }
+    }
+
+    public static int getLargest(int[] a, int total){
+        int temp;
+        for (int i = 0; i < total; i++)
+        {
+            for (int j = i + 1; j < total; j++)
+            {
+                if (a[i] > a[j])
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        return a[total-1];
     }
 }
